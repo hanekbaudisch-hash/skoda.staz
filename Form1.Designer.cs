fcf581c9215afa467bddd82deeb8536a3d147e8a
Cupik <Cupik@DC-BADY-DELL>
2026-04-19 21:36:33 +0200

    Přidejte soubory projektu.

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace skoda.staz
+{
+    partial class Form1
+    {
+
+        private System.ComponentModel.IContainer components = null;
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+
+        private void InitializeComponent()
+        {
+            this.btnPlan = new System.Windows.Forms.Button();
+            this.btnSklad = new System.Windows.Forms.Button();
+            this.btnDodavatel = new System.Windows.Forms.Button();
+            this.btnGenerovat = new System.Windows.Forms.Button();
+            this.dgvVysledky = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVysledky)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPlan
+            // 
+            this.btnPlan.BackColor = System.Drawing.Color.Ivory;
+            this.btnPlan.Location = new System.Drawing.Point(129, 62);
+            this.btnPlan.Name = "btnPlan";
+            this.btnPlan.Size = new System.Drawing.Size(138, 37);
+            this.btnPlan.TabIndex = 0;
+            this.btnPlan.Text = "Načíst plán";
+            this.btnPlan.UseVisualStyleBackColor = false;
+            this.btnPlan.Click += new System.EventHandler(this.btnPlan_Click);
+            // 
+            // btnSklad
+            // 
+            this.btnSklad.BackColor = System.Drawing.Color.Ivory;
+            this.btnSklad.Location = new System.Drawing.Point(129, 127);
+            this.btnSklad.Name = "btnSklad";
+            this.btnSklad.Size = new System.Drawing.Size(138, 37);
+            this.btnSklad.TabIndex = 1;
+            this.btnSklad.Text = "Načíst sklad";
+            this.btnSklad.UseVisualStyleBackColor = false;
+            this.btnSklad.Click += new System.EventHandler(this.btnSklad_Click);
+            // 
+            // btnDodavatel
+            // 
+            this.btnDodavatel.BackColor = System.Drawing.Color.Ivory;
+            this.btnDodavatel.Location = new System.Drawing.Point(129, 194);
+            this.btnDodavatel.Name = "btnDodavatel";
+            this.btnDodavatel.Size = new System.Drawing.Size(138, 37);
+            this.btnDodavatel.TabIndex = 2;
+            this.btnDodavatel.Text = "Načíst dodavatele";
+            this.btnDodavatel.UseVisualStyleBackColor = false;
+            this.btnDodavatel.Click += new System.EventHandler(this.btnDodavatel_Click);
+            // 
+            // btnGenerovat
+            // 
+            this.btnGenerovat.BackColor = System.Drawing.Color.Ivory;
+            this.btnGenerovat.Location = new System.Drawing.Point(129, 251);
+            this.btnGenerovat.Name = "btnGenerovat";
+            this.btnGenerovat.Size = new System.Drawing.Size(138, 37);
+            this.btnGenerovat.TabIndex = 3;
+            this.btnGenerovat.Text = "Generovat";
+            this.btnGenerovat.UseVisualStyleBackColor = false;
+            this.btnGenerovat.Click += new System.EventHandler(this.btnGenerovat_Click);
+            // 
+            // dgvVysledky
+            // 
+            this.dgvVysledky.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvVysledky.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvVysledky.GridColor = System.Drawing.Color.LightGreen;
+            this.dgvVysledky.Location = new System.Drawing.Point(352, 62);
+            this.dgvVysledky.Name = "dgvVysledky";
+            this.dgvVysledky.ReadOnly = true;
+            this.dgvVysledky.RowHeadersWidth = 51;
+            this.dgvVysledky.RowTemplate.Height = 24;
+            this.dgvVysledky.Size = new System.Drawing.Size(1119, 504);
+            this.dgvVysledky.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(124, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 32);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Načti soubory";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.SkyBlue;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(347, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 32);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Výsledný report";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(1475, 568);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvVysledky);
+            this.Controls.Add(this.btnGenerovat);
+            this.Controls.Add(this.btnDodavatel);
+            this.Controls.Add(this.btnSklad);
+            this.Controls.Add(this.btnPlan);
+            this.Name = "Form1";
+            this.Text = "Správa údržby letadel";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVysledky)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlan;
+        private System.Windows.Forms.Button btnSklad;
+        private System.Windows.Forms.Button btnDodavatel;
+        private System.Windows.Forms.Button btnGenerovat;
+        private System.Windows.Forms.DataGridView dgvVysledky;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using ClosedXML.Excel;
+
+namespace skoda.staz
+{
+    public partial class Form1 : Form
+    {
+        string cestaPlan = "";
+        string cestaSklad = "";
+        string cestaDodavatele = ""; 
+
+        public Form1()
+        {
+            InitializeComponent(); 
+        }
+
+        private string VyberSoubor() //vybira pouze excel soubory
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Excel (*.xlsx)|*.xlsx";
+                return ofd.ShowDialog() == DialogResult.OK ? ofd.FileName : "";
+            }
+        } 
+
+        private DataTable NactiExcel(string cesta)
+        {
+            var dt = new DataTable();
+
+            using (var wb = new XLWorkbook(cesta))
+            {
+                var ws = wb.Worksheet(1);
+                bool firstRow = true;
+
+                foreach (var row in ws.RowsUsed())
+                {
+                    if (firstRow)
+                    {
+                        foreach (var cell in row.Cells())
+                            dt.Columns.Add(cell.Value.ToString().Trim());
+                        firstRow = false;
+                    }
+                    else
+                    {
+                        dt.Rows.Add(row.Cells().Select(c => c.Value.ToString().Trim()).ToArray());
+                    }
+                }
+            }
+            return dt;
+        }
+
+        private bool ZkontrolujSloupce(DataTable dt, params string[] sloupce) //kontroluje zda dany exccel obsahuje potrebne sloupce
+        {
+            foreach (var s in sloupce)
+            {
+                if (!dt.Columns.Contains(s))
+                    return false;
+            }
+            return true;
+        }
+
+        private List<VystupniRadek> AnalyzujData(DataTable dtPlan, DataTable dtSklad, DataTable dtDodavatele) //vysledna tabulka a sloupce
+        {
+            var report = new List<VystupniRadek>();
+
+            foreach (DataRow r in dtPlan.Rows)
+            {
+                string kod = r["Kod_Soucastky"].ToString();
+
+                var radek = new VystupniRadek
+                {
+                    ID_Letadla = r["ID_Letadla"].ToString(),
+                    Kod_Soucastky = kod
+                };
+
+                var sklad = dtSklad.AsEnumerable()
+                    .FirstOrDefault(s =>
+                        s["Kod_Soucastky"].ToString() == kod &&
+                        int.TryParse(s["Skladem_ks"].ToString(), out int ks) && ks > 0);
+
+                if (sklad != null)
+                {
+                    radek.Stav = "Skladem";
+                    radek.DodavatelUmisteni = sklad["Umisteni"].ToString();
+                    radek.Cena = "0 CZK";
+                    radek.Termin_Dodani = "Ihned";
+                }
+                else
+                {
+                    var dod = dtDodavatele.AsEnumerable()
+                        .Where(d => d["Kod_Soucastky"].ToString() == kod)
+                        .OrderBy(d =>
+                        {
+                            double.TryParse(d["Cena_CZK"].ToString(), out double cena);
+                            return cena;
+                        })
+                        .FirstOrDefault();
+
+                    if (dod != null)
+                    {
+                        radek.Stav = "Objednat";
+                        radek.DodavatelUmisteni = dod["Dodavatel"].ToString();
+                        radek.Cena = dod["Cena_CZK"] + " CZK";
+                        radek.Termin_Dodani = dod["Dodani_Dny"] + " dní";
+                    }
+                    else
+                    {
+                        radek.Stav = "Nenalezeno";
+                        radek.DodavatelUmisteni = "Chybí data";
+                        radek.Cena = "-";
+                        radek.Termin_Dodani = "-";
+                    }
+                }
+
+                report.Add(radek);
+            }
+
+            return report;
+        }
+
+        private void btnPlan_Click(object sender, EventArgs e)
+        {
+            cestaPlan = VyberSoubor();
+        }
+
+        private void btnSklad_Click(object sender, EventArgs e)
+        {
+            cestaSklad = VyberSoubor();
+        }
+
+        private void btnDodavatel_Click(object sender, EventArgs e)
+        {
+            cestaDodavatele = VyberSoubor();
+        }
+
+        private void btnGenerovat_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(cestaPlan) ||
+                    string.IsNullOrEmpty(cestaSklad) ||
+                    string.IsNullOrEmpty(cestaDodavatele))
+                {
+                    MessageBox.Show("Vyber všechny soubory!");
+                    return;
+                }
+
+                var dtPlan = NactiExcel(cestaPlan);
+                var dtSklad = NactiExcel(cestaSklad);
+                var dtDod = NactiExcel(cestaDodavatele);
+
+                if (!ZkontrolujSloupce(dtPlan, "Kod_Soucastky", "ID_Letadla") ||
+                    !ZkontrolujSloupce(dtSklad, "Kod_Soucastky", "Skladem_ks", "Umisteni") ||
+                    !ZkontrolujSloupce(dtDod, "Kod_Soucastky", "Cena_CZK", "Dodavatel", "Dodani_Dny"))
+                {
+                    MessageBox.Show("Některý soubor nemá správné sloupce!");
+                    return;
+                }
+
+                var vysledky = AnalyzujData(dtPlan, dtSklad, dtDod);
+
+                dgvVysledky.DataSource = vysledky;
+
+                using (SaveFileDialog sfd = new SaveFileDialog())
+                {
+                    sfd.Filter = "Excel (*.xlsx)|*.xlsx";
+                    sfd.FileName = "finalni_report.xlsx";
+
+                    if (sfd.ShowDialog() == DialogResult.OK)
+                    {
+                        UlozDoExcelu(vysledky, sfd.FileName);
+                        MessageBox.Show("Report uložen!");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Chyba: " + ex.Message);
+            }
+        }
+
+        private void UlozDoExcelu(List<VystupniRadek> data, string cesta)
+        {
+            using (var wb = new XLWorkbook())
+            {
+                var ws = wb.Worksheets.Add("Report");
+
+                ws.Cell(1, 1).Value = "ID_Letadla";
+                ws.Cell(1, 2).Value = "Kod_Soucastky";
+                ws.Cell(1, 3).Value = "Stav";
+                ws.Cell(1, 4).Value = "Dodavatel/Umisteni";
+                ws.Cell(1, 5).Value = "Cena";
+                ws.Cell(1, 6).Value = "Termin_Dodani";
+
+                for (int i = 0; i < data.Count; i++)
+                {
+                    ws.Cell(i + 2, 1).Value = data[i].ID_Letadla;
+                    ws.Cell(i + 2, 2).Value = data[i].Kod_Soucastky;
+                    ws.Cell(i + 2, 3).Value = data[i].Stav;
+                    ws.Cell(i + 2, 4).Value = data[i].DodavatelUmisteni;
+                    ws.Cell(i + 2, 5).Value = data[i].Cena;
+                    ws.Cell(i + 2, 6).Value = data[i].Termin_Dodani;
+                }
+
+                ws.Columns().AdjustToContents();
+                wb.SaveAs(cesta);
+            }
+        }
+
+    }
+
+    public class VystupniRadek
+    {
+        public string ID_Letadla { get; set; }
+        public string Kod_Soucastky { get; set; }
+        public string Stav { get; set; }
+        public string DodavatelUmisteni { get; set; }
+        public string Cena { get; set; }
+        public string Termin_Dodani { get; set; }
+    }
+}
